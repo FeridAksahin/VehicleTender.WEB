069a606d2ee08762a4fa5543bf66c8d959679182
Okan Coşar <okancosar1@hotmail.com>
2022-10-02 18:27:09 +0300

    login, register and forgot password pages done for user uı

--- VehicleTender.Web.EndUserUI/App_Start/BundleConfig.cs
+++ VehicleTender.Web.EndUserUI/App_Start/BundleConfig.cs
@@ -19,8 +19,8 @@ namespace VehicleTender.Web.EndUserUI
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
