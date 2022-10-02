64583f2a761fc0f38951fa0ce0243277548ec2f7
iihsan <iiihsank@gmail.com>
2022-10-02 22:53:24 +0300

    Update

--- VehicleTender.Web.AdminUI/Controllers/DenemeController.cs
+++ VehicleTender.Web.AdminUI/Controllers/DenemeController.cs
@@ -4,14 +4,7 @@ namespace VehicleTender.Web.AdminUI.Controllers
 {
     public class DenemeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-        public IActionResult Index01()
-        {
-            return View();
-        }
+
         public IActionResult AdminHome()
         {
             return View();
