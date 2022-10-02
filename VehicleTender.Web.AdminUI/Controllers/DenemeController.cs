b58c325db6f8bb24d4833e3d1a0175a1eac2e832
iihsan <iiihsank@gmail.com>
2022-10-02 22:52:25 +0300

    ErrorPage

--- VehicleTender.Web.AdminUI/Controllers/DenemeController.cs
+++ VehicleTender.Web.AdminUI/Controllers/DenemeController.cs
@@ -20,5 +20,9 @@ namespace VehicleTender.Web.AdminUI.Controllers
         {
             return View();
         }
+        public IActionResult Error()
+        {
+            return View();
+        }
     }
 }
