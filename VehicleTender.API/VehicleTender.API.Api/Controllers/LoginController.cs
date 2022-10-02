36992d5d4b3dc31c3f929a58186e4298fadc8541
Can İçcan <95756970+caniccan@users.noreply.github.com>
2022-10-02 20:27:07 +0300

    Controllers created

---
+++ VehicleTender.API/VehicleTender.API.Api/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VehicleTender.API.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        /// <summary>
+        /// All Login actions will be done here.
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ VehicleTender.API/VehicleTender.API.Api/Controllers/ReportController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VehicleTender.API.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : Controller
+    {
+        /// <summary>
+        /// All Report actions will be done here.
+        /// </summary>
+        /// <returns></returns>
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
