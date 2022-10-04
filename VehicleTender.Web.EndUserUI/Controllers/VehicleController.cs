67bfb7e08936a4460f1986d5e598204ad5f2e86b
Okan Coşar <cosaarokan@gmail.com>
2022-10-04 12:24:20 +0300

    added vehicle controller and view

---
+++ VehicleTender.Web.EndUserUI/Controllers/VehicleController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VehicleTender.Web.EndUserUI.Controllers
+{
+    public class VehicleController : Controller
+    {
+        // GET: Vehicle
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListVehicle()
+        {
+            return View();
+        }
+    }
+}
