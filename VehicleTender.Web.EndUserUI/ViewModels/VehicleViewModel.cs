ccf10ef72869b5f73b18c1e16e0467797300e34b
Okan Coşar <cosaarokan@gmail.com>
2022-10-05 09:34:57 +0300

    some api methods changed

---
+++ VehicleTender.Web.EndUserUI/ViewModels/VehicleViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VehicleTender.Web.EndUserUI.ViewModels
+{
+    public class VehicleViewModel
+    {
+        public List<VehicleListViewModel> VehicleList { get; set; }
+        public string BrandId { get; set; }
+        public string ModelId { get; set; }
+        public bool? isIndividual { get; set; }
+        public string StatusId { get; set; }
+    }
+}
