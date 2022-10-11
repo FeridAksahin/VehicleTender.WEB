0971d4434c62dba49f31e416bd11c8333e6866e4
Dogukan Matuloglu <dogukan.matul@gmail.com>
2022-10-11 14:37:33 +0300

    Advert Controller Added

---
+++ VehicleTender.Web.EndUserUI/Controllers/AdvertController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VehicleTender.Web.EndUserUI.Controllers
+{
+    public class AdvertController : Controller
+    {
+        // GET: Advert
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
