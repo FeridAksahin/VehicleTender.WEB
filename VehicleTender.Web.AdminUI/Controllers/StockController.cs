293f27f15e91c77df2b7f66f70a8ba5c4fd540c3
Dogukan Matuloglu <dogukan.matul@gmail.com>
2022-10-11 15:02:01 +0300

    Stock Controller Added

---
+++ VehicleTender.Web.AdminUI/Controllers/StockController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VehicleTender.Web.AdminUI.Controllers
+{
+    public class StockController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Detail(int id)
+        {
+            return View();
+        }
+    }
+}
