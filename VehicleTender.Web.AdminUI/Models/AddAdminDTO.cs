bac43adc7082c53534b46551e76e791e3f803da9
Ferid Akşahin <ferid.aksahin98@gmail.com>
2022-10-12 11:16:40 +0300

    Dto writed for admin project

---
+++ VehicleTender.Web.AdminUI/Models/AddAdminDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace VehicleTender.Web.AdminUI.Models
+{
+    public class AddAdminDTO
+    {
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Telephone { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool isActive { get; set; }
+    }
+}
---
+++ VehicleTender.Web.AdminUI/Models/GetCarDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace VehicleTender.Web.AdminUI.Models
+{
+    public class GetCarDTO
+    {
+        public int CarId { get; set; }
+        public string CarBrand { get; set; }
+        public string CarModel { get; set; }
+        public string IndividualOrCorparate { get; set; }
+        public string Statu { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
