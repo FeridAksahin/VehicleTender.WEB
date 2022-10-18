8ff1ae8369a3483a520572d5869fb0434abfe0a3
Ferid Akşahin <ferid.aksahin98@gmail.com>
2022-10-18 23:31:26 +0300

    Dto has been injected into the Admin (UserOperation) controller and view.

--- VehicleTender.Web.AdminUI/Controllers/UserOperationsController.cs
+++ VehicleTender.Web.AdminUI/Controllers/UserOperationsController.cs
@@ -1,17 +1,91 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VehicleTender.Web.AdminUI.ApiServices.Services;
+using VehicleTender.Web.AdminUI.Models.Admin;
+using VehicleTender.Web.AdminUI.Models.PageModel;
+using VehicleTender.Web.AdminUI.Models.Token;
 
 namespace VehicleTender.Web.AdminUI.Controllers
 {
     public class UserOperationsController : Controller
     {
-        public IActionResult UserList()
+        AdminService adminService = new AdminService();
+        BearerTokenDTO token = new BearerTokenDTO(); //durumluk 
+        [HttpGet]
+        public async Task<IActionResult> UserList()
         {
-            return View();
+            AdminsPage adminsPageModel = new AdminsPage();
+            //adminsPageModel.getAdminDTO = await adminService.GetAllAdmin(token);  -- db den çekilmediginde null hatası veriyor 
+            List<GetAdminDTO> listAdmin = new List<GetAdminDTO>();
+            GetAdminDTO testlik = new GetAdminDTO(); //o yüzden testlik 
+            testlik.isActive = true;
+            testlik.Telephone = "gearg";
+            testlik.Email = "asdf";
+            testlik.Surname = "vs";
+            testlik.Username = "agaraerg";
+            testlik.UserId = 4;
+            testlik.Name = "gagarge";
+            listAdmin.Add(testlik);
+            adminsPageModel.getAdminDTO = listAdmin;
+            return View(adminsPageModel);
         }
+        [HttpPost]
+        public async Task<IActionResult> updateTest(string id)
+        {
+            var a = adminService.GetAdminById(token, id);
+            GetAdminDTO testlik = new GetAdminDTO();
+            testlik.isActive = true;
+            testlik.Telephone = "gearg";
+            testlik.Email = "asdf";
+            testlik.Surname = "vs";
+            testlik.Username = "agaraerg";
+            testlik.UserId = 4;
+            testlik.Name = "gagarge";
+            return RedirectToAction("UserList");
+        }
+        /*
+        [HttpPost]
+        public async Task<IActionResult> UserList(string id)
+        {
+            AdminsPage adminsPageModele = new AdminsPage();
+            //adminService.GetAdminById(token, id);
 
+            //  adminsPageModel.getAdminDTO = await adminService.GetAllAdmin(token);  -- db den çekilmediginde null hatası veriyor 
+
+            return RedirectToAction("",adminsPageModele);
+        }
+        */
+        [HttpPost]
+        public IActionResult AddNewAdmin(AdminsPage admin)
+        {
+            var a = 4;
+            Console.WriteLine(admin.getAdminDTO);
+            adminService.AddNewAdmin(token, admin.addAdminDTO);
+            return RedirectToAction("UserList");
+        }
+        
+        [HttpPost]
+        public IActionResult DeleteAdmin(int id)
+        {
+            var a = 4;
+            Console.WriteLine(id);
+            adminService.DeleteAdmin(token, id);
+            return RedirectToAction("UserList");
+        }
+        /*
+        [HttpPost]
+        public IActionResult UpdateAdmin(int id)
+        {
+            var a = 4;
+            Console.WriteLine(id);
+            return RedirectToAction("UserList");
+        }*/
+
+        /*
+        [HttpGet]
         public IActionResult PackageIdentification()
         {
             return View();
-        }
+        }*/
+
     }
 }
---
+++ VehicleTender.Web.AdminUI/Models/PageModel/AdminsPage.cs
@@ -0,0 +1,12 @@
+﻿using VehicleTender.Web.AdminUI.Models.Admin;
+
+namespace VehicleTender.Web.AdminUI.Models.PageModel
+{
+    public class AdminsPage
+    {
+        public AddAdminDTO addAdminDTO { get; set; }
+        public List<GetAdminDTO> getAdminDTO { get; set; }  
+        public UpdateAdmin updateAdmin { get; set; }  
+
+    }
+}
