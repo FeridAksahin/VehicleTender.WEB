e98e12ba797278d9f4927717090628535c3721db
Can İçcan <95756970+caniccan@users.noreply.github.com>
2022-10-19 21:27:28 +0300

    Commission page's controller,dto and view updated.

---
+++ VehicleTender.Web.AdminUI/Models/PageModel/CommissionPageModel.cs
@@ -0,0 +1,11 @@
+﻿using VehicleTender.Web.AdminUI.Models.Commission;
+
+namespace VehicleTender.Web.AdminUI.Models.PageModel
+{
+    public class CommissionPageModel
+    {
+        public CommissionDTO AddCommission { get; set; }
+        public List<CommissionDTO> CommissionList { get; set; }
+        public UpdateCommission UpdateCommission { get; set; }
+    }
+}
