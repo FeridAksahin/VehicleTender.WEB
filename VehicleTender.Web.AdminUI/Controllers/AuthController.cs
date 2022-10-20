62f9f834c1fe8b4f70cca57f0cc6444f3ee82291
iihsan <iiihsank@gmail.com>
2022-10-20 20:10:43 +0300

    Add_Login

--- VehicleTender.Web.AdminUI/Controllers/AuthController.cs
+++ VehicleTender.Web.AdminUI/Controllers/AuthController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VehicleTender.Web.AdminUI.Models;
+using VehicleTender.Web.AdminUI.Models.Login;
 
 namespace VehicleTender.Web.AdminUI.Controllers
 {
@@ -10,6 +12,16 @@ namespace VehicleTender.Web.AdminUI.Controllers
     {
         public IActionResult Login()
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(UserLoginModel user)
+        {
+            //if (true)
+            //{
+            //    CookieProccess cookie = new CookieProccess(HttpContext.);
+            //    return RedirectToAction("Admin", "AdminHome");
+            //}
             return View();
         }
         public IActionResult Register()
