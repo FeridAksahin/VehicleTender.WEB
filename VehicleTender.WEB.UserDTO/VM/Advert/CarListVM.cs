faa08bb52aa48f6b9ed56989207204fab8fbeec5
Okan Coşar <cosaarokan@gmail.com>
2022-10-22 17:25:02 +0300

    home controller done, nontenderVehicle controller done

--- VehicleTender.WEB.UserDTO/VM/Advert/CarListVM.cs
+++ VehicleTender.WEB.UserDTO/VM/Advert/CarListVM.cs
@@ -2,13 +2,20 @@
 {
     public class CarListVM
     {
+        public int CarId { get; set; }
         public int BrandId { get; set; }
+        public string BrandName { get; set; }
         public int ModelId { get; set; }
+        public string ModelName { get; set; }
         public int BodyTypeId { get; set; }
+        public string BodyTypeName { get; set; }
         public int Year { get; set; }
         public int GearTypeId { get; set; }
+        public string GearTypeName { get; set; }
         public int FuelTypeId { get; set; }
+        public string FuelTypeName { get; set; }
         public int ColorId { get; set; }
+        public string ColorName { get; set; }
         public string Version { get; set; }
         public string HardwareId { get; set; }
         public string Kilometer { get; set; }
--- VehicleTender.Web.EndUserUI/Controllers/NonTenderVehicleController.cs
+++ VehicleTender.Web.EndUserUI/Controllers/NonTenderVehicleController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VehicleTender.WEB.UserDTO.VM.Advert;
+using VehicleTender.WEB.UserDTO.VM.Page;
 
 namespace VehicleTender.Web.EndUserUI.Controllers
 { //hemen araç al (bireysel araç satıştan gelen araçlara alım-satım controllerı)
@@ -12,12 +14,57 @@ namespace VehicleTender.Web.EndUserUI.Controllers
         [HttpGet]
         public ActionResult BuyAVehicleNow()
         {
-            return View();
+            BuyAVehicleNowVM advertPage = new BuyAVehicleNowVM();
+            List<CarListVM> carList = new List<CarListVM>();
+            CarListVM car = new CarListVM()
+            {
+                CarId = 1,
+                BrandName = "Mercedes",
+                ModelName = "CLA",
+                Kilometer = "2500",
+                GearTypeName = "Otomatik",
+                ColorName = "Siyah",
+                FuelTypeName = "Benzinli",
+                HardwareId = "Turbo",
+                Version = "Yeni",
+                BodyTypeName = "Sedan",   
+                Year = 2015,
+                Description = "Acil satılık, az kullanılmış aynı zamanda krediye de uygundur."
+            };
+
+            carList.Add(car);
+            advertPage.Cars = carList;
+
+            return View(advertPage);
         }
-        [HttpGet]
-        public ActionResult VehicleDetail()
+
+        [HttpPost]
+        public ActionResult BuyAVehicleNow(BuyAVehicleNowVM advertPage)
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult VehicleDetail(int id)
+        {
+            List<CarListVM> carList = new List<CarListVM>();
+            CarListVM car = new CarListVM()
+            {
+                CarId = 1,
+                BrandName = "Mercedes",
+                ModelName = "CLA",
+                Kilometer = "2500",
+                GearTypeName = "Otomatik",
+                ColorName = "Siyah",
+                FuelTypeName = "Benzinli",
+                HardwareId = "Turbo",
+                Version = "Yeni",
+                BodyTypeName = "Sedan",
+                Year = 2015,
+                Description = "Acil satılık, az kullanılmış aynı zamanda krediye de uygundur."
+            };
+            carList.Add(car);
+            return View(carList);
         }
 
     }
